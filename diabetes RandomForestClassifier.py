5c801f2cb62e366bbacecd72fc730a6bd49cfc45
shre30 <94737493+shre30@users.noreply.github.com>
2021-11-24 22:51:38 +0530

    Add files via upload

---
+++ diabetes RandomForestClassifier.py
@@ -0,0 +1,16 @@
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import numpy as np
+diabetes = pd.read_csv('diabetes.csv')
+
+X_train, X_test, y_train, y_test = train_test_split(diabetes.loc[:, diabetes.columns != 'Outcome'], diabetes['Outcome'], stratify=diabetes['Outcome'], random_state=66)
+
+from sklearn.ensemble import RandomForestClassifier
+rf = RandomForestClassifier(n_estimators=100, random_state=0)
+rf.fit(X_train, y_train)
+print("Accuracy on training set: {:.3f}".format(rf.score(X_train, y_train)))
+print("Accuracy on test set: {:.3f}".format(rf.score(X_test, y_test)))
+rf1 = RandomForestClassifier(max_depth=3, n_estimators=100, random_state=0)
+rf1.fit(X_train, y_train)
+print("Accuracy on training set: {:.3f}".format(rf1.score(X_train, y_train)))
+print("Accuracy on test set: {:.3f}".format(rf1.score(X_test, y_test)))
